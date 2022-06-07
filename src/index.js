6b12070fe11aceca12a1b299800765b1e2a5571a
Sharlevych Dmytro <sharlevychdmytro@gmail.com>
2022-06-07 21:00:28 +0300

    add render popular movies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './js/advice&team-modal/create-advice-form';
 import './js/advice&team-modal/advice-films-by-ganre';
 import './js/advice&team-modal/open-close-team-modal';
-import './js/render-popular-movies';
+import './js/popular-movies/index';
 import './js/advice&team-modal/advertising';
 import { homeModalJS } from './home-modal-js/homeModalJS';
-import './js/keyword-search-film/keyword-search-film'
+import './js/keyword-search-film/keyword-search-film';
 
 homeModalJS();
