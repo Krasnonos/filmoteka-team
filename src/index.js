d9b6af6a4da08e1455aa7f6196042c1f56654f8d
Krasnonos <andreykrasnonos@icloud.com>
2022-06-07 15:08:27 +0300

    ready advertising

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './js/advice&team-modal/create-advice-form';
 import './js/advice&team-modal/advice-films-by-ganre';
 import './js/render-popular-movies';
+import './js/advice&team-modal/advertising';
 import { homeModalJS } from './home-modal-js/homeModalJS';
 
 homeModalJS();
---
+++ src/js/advice&team-modal/advertising.js
@@ -0,0 +1,49 @@
+const addImageArray = [
+  'https://re-media.plektan.com/img/prop/40/7f/407f503405e4ad187970a5873d2756a0.jpg',
+  'https://ochepyatki.ru/upfiles/albums/1907/f_46550.jpg',
+];
+
+const addBlockEl = document.querySelector('.add-block');
+const addBlockTime = document.querySelector('.add-block__time');
+const addBlockBtn = document.querySelector('.add-block__btn');
+
+(function renderAddImg(addImageArray) {
+  const minNumber = 0;
+  const maxNumber = addImageArray.length - 1;
+  const randomimgIndex = Math.round(
+    Math.random() * (maxNumber - minNumber) + minNumber
+  );
+
+  const imageEl = `<img
+    src="${addImageArray[randomimgIndex]}"
+    alt="advertising"
+    class="add-block__image"
+  />`;
+
+  addBlockEl.insertAdjacentHTML('beforeend', imageEl);
+})(addImageArray);
+
+(function deadLineTimer() {
+  const startTime = Date.now();
+  let diferentTimeSec = 0;
+
+  const intervalId = setInterval(() => {
+    if (diferentTimeSec >= 10) {
+      clearInterval(intervalId);
+      addBlockTime.classList.add('is-hidden');
+      addBlockBtn.classList.remove('is-hidden');
+
+      return;
+    }
+
+    const currentTime = Date.now();
+    const diferentTimeMs = currentTime - startTime;
+
+    diferentTimeSec = (diferentTimeMs / 1000).toFixed();
+    addBlockTime.textContent = diferentTimeSec;
+  }, 1000);
+})();
+
+addBlockBtn.addEventListener('click', () => {
+  addBlockEl.classList.add('is-hidden');
+});
