56a3046b0d4b49ba200475a3ec9c7d7bf59b64fb
Krasnonos <andreykrasnonos@icloud.com>
2022-06-07 12:23:14 +0300

    finish advice-form

--- src/js/advice&team-modal/create-advice-form.js
+++ src/js/advice&team-modal/create-advice-form.js
@@ -1,13 +1,40 @@
-import { genreIds } from '../ganre-ids';
+// import { genreIds } from './ganre-ids-for-advice';
 
-const adviceForm = document.querySelector('select[id="ganre"]');
+// const adviceListEl = document.querySelector('.advice-list');
 
-function createSelectBtn(genreIds) {
-  return genreIds
-    .map(({ id, name }) => {
-      return `<option value="${id}">${name}</option>`;
-    })
-    .join('');
+// function createSelectBtn(genreIds) {
+//   return genreIds
+//     .map(({ id, name }) => {
+//       return `<li class="advice-list__item"> <button type="button" class="advice-list__btn" data-id="${id}">${name}</button></li>`;
+//     })
+//     .join('');
+// }
+
+// adviceListEl.insertAdjacentHTML('beforeend', createSelectBtn(genreIds));
+
+{
+  /* <div class="advice-block">
+  <p class="advice-block__text">
+    Repudiandae, debitis facere dignissimos omnis sit, aspernatur.
+  </p>
+  <div class="advice-list__wrap">
+    <button
+      type="button"
+      class="advice-list__nav-btn advice-list__nav-btn--left"
+    >
+      <svg class="advice-list__svg" width="30" height="30">
+        <use href="./images/icons.svg#icon-undo-left"></use>
+      </svg>
+    </button>
+    <ul class="advice-list"></ul>
+    <button
+      type="button"
+      class="advice-list__nav-btn advice-list__nav-btn--right"
+    >
+      <svg class="advice-list__svg" width="30" height="30">
+        <use href="./images/icons.svg#icon-redo-right"></use>
+      </svg>
+    </button>
+  </div>
+</div>; */
 }
-
-adviceForm.insertAdjacentHTML('afterbegin', createSelectBtn(genreIds));
