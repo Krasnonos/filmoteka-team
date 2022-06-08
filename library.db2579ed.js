569eb3bb780b72edaaee29ca0cb1dcb23c769b11
marianalevitska <92450123+marianalevitska@users.noreply.github.com>
2022-06-08 11:45:45 +0000

    Deploying to gh-pages from @ Krasnonos/filmoteka-team@c9f347a4749a1c9737cc91a571fdbe372db2919b 🚀

---
+++ library.db2579ed.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".footer__btn"),t=document.querySelector(".modal__close-btn"),c=document.querySelector(".backdrop-team");function n(){c.classList.remove("is-hidden"),t.addEventListener("click",i),e.removeEventListener("click",n)}function i(){c.classList.add("is-hidden"),e.addEventListener("click",n),t.removeEventListener("click",i)}e.addEventListener("click",n);
+//# sourceMappingURL=library.db2579ed.js.map
