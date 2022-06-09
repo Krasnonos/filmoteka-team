b58fd0a874c99a1ecc91bc213d34979f97664178
benzotrav <93350704+benzotrav@users.noreply.github.com>
2022-06-10 00:13:42 +0300

    task done

---
+++ src/js/home-modal-js/closingModal.js
@@ -0,0 +1,21 @@
+import { refs } from '../refs';
+
+
+
+  window.addEventListener('keydown', event => { 
+    if (event.code === "Escape") { 
+        refs.modalBackdropRef.classList.add('is-hidden');
+        refs.bodyRef.classList.remove('modal-open');
+        const modalRef = document.querySelector('.modal__box .modal');
+        modalRef.remove();
+    };
+  });
+
+document.addEventListener('click', event => {
+ if (event.target === refs.modalBackdropRef) {
+  refs.modalBackdropRef.classList.add('is-hidden');
+  refs.bodyRef.classList.remove('modal-open');
+  const modalRef = document.querySelector('.modal__box .modal');
+  modalRef.remove();
+ }; 
+});
