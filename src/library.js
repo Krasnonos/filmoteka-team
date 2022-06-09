a977d5f0c055245a18b86603c3d2101d72a79f3a
Krasnonos <andreykrasnonos@icloud.com>
2022-06-09 13:05:17 +0300

    almost done clear one card

--- src/library.js
+++ src/library.js
@@ -1,5 +1,5 @@
-
-// import './js/clear-local-storage-library.js/change-data-atr-on-nav-btn'
+// import './js/clear-local-storage-library.js/change-current-page'
+// import './js/clear-local-storage-library.js/clear-local-storage'
+// import './js/clear-local-storage-library.js/clear-one-card';
 import './js/spinner-js/spinner';
 import './js/advice&team-modal/open-close-team-modal';
-
