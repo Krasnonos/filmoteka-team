aaaf960c23528aae57f18b3f9acd0ad3699ad7af
Sharlevych Dmytro <sharlevychdmytro@gmail.com>
2022-06-13 21:35:05 +0300

    X

--- src/js/my-library-home/index.js
+++ src/js/my-library-home/index.js
@@ -1,10 +1,3 @@
-// 1) Отримати посилання на DOM елементи. +
-// 2) Стукаємося до LocalStorage і отримуємо масив id. +
-// 3) Робимо запит на сервер по ids.
-// 4) Отримуємо данні з сервера.
-// 5) Рендерим розмітку карток фільмів.
-// 6) Записуємо данні з сервера для подальшого використання карток.
-// 7) Додатково зробити перевірку, якщо LocalStorage порожній рендерим текст, що переглянутих фільмів ще немає у Library.
 import { getLocalStorageWatched } from './get-local-storage';
 import { requestIds } from './request-ids';
 import { createMarkup } from './create-markup';
