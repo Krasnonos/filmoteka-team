3367fe1a43dd8ccaece2a6478b5280a64571f969
Krasnonos <andreykrasnonos@icloud.com>
2022-06-13 14:03:18 +0300

    ready clear local-storage

--- src/library.js
+++ src/library.js
@@ -1,6 +1,6 @@
-// import './js/clear-local-storage-library.js/change-current-page'
-// import './js/clear-local-storage-library.js/clear-local-storage'
+import './js/clear-local-storage-library.js/change-current-page';
 // import './js/clear-local-storage-library.js/clear-one-card';
 import './js/my-library/my-library';
 import './js/spinner-js/spinner';
 import './js/advice&team-modal/open-close-team-modal';
+import './js/clear-local-storage-library.js/clear-local-storage';
