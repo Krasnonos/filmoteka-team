521ad23af0aea3e969bea518d0dc789575fd93e2
Olga-94 <olgabezdetko1994@gmail.com>
2022-06-13 21:25:33 +0300

    delet comment

--- src/js/pagination/pagination.js
+++ src/js/pagination/pagination.js
@@ -28,18 +28,12 @@ async function ready(page = 1) {
 }
 ready();
 function renderPagination (page, totalPage) {
-  // const linkArrowLeftRef = document.querySelector('.pagination__arrow--left');
-  // const linkArrowRightRef = document.querySelector('.pagination__arrow--right');
-  // totalPage = 20;
   currentPage = page;
 let paginationMarkup = '';
 let beforeTwoPage = page - 2;
 let beforePage = page - 1;
 let afterTwoPage = page + 2;
 let afterPage = page + 1;
-// paginationNumberListRef.addEventListener('click', ready);
-//   linkArrowRightRef.addEventListener('click', ready);
-//   linkArrowLeftRef.addEventListener('click', ready);
 
 if (page > 1) {
   paginationMarkup += `<li class="pagination__arrow pagination__arrow--left materials-icons">&larr;</li>`
@@ -82,9 +76,7 @@ if (page < totalPage) {
 paginationList.innerHTML = paginationMarkup;
  
 paginationList.addEventListener('click', onPagination);
-// paginationNumberListRef.addEventListener('click', onPagination);
-  // linkArrowRightRef.addEventListener('click', onPagination);
-  // linkArrowLeftRef.addEventListener('click', onPagination);
+
 
 }
 
@@ -107,90 +99,8 @@ case '...':
   break;
 
  default: ready(target);
-
  
 }
 } 
 
 
-
-
-
-// export function pagination() {
-// const linkArrowLeftRef = document.querySelector('.pagination__arrow--left');
-//   const linkArrowRightRef = document.querySelector('.pagination__arrow--right');
-//   const paginationNumberListRef = document.querySelector('.pagination__number-list');
-
-// let current_page =1;
-// let pageNum = 5;
-
-
-// }
-// const per_page_max = 1000; // Максимальное кол-во страниц
-// let current_page = 5; // Текущая страница
-// const btns = document.querySelector('.pagination__number-list');
-
-// function appendBtn(i, ellipsis) {
-//   const activeBtn = current_page === i;
-//   const button = document.createElement('button');
-//   button.classList.add('pagination__number');
-//   if (ellipsis === true) {
-//     button.innerHTML = '...';
-//     button.disabled = true;
-//     btns.append(button);
-//     return false;
-//   }
-//   if (activeBtn) {
-//     button.classList.add('pagination__number--active');
-//   }
-//   button.disabled = activeBtn;
-//   button.innerHTML = i;
-//   button.addEventListener('click', () => {
-//     current_page = i;
-//     btns.innerHTML = '';
-//     logic();
-//   });
-//   btns.append(button);
-
-// }
- 
-// logic();
-
-// function logic() {
-
-//   // это лево
-//   appendBtn(1);
-//   appendBtn(2);
-//   // это лево
-
-//   // это середина
-//   if (current_page < 6) {
-//     appendBtn(3);
-//     appendBtn(4);
-//     appendBtn(5);
-//     appendBtn(6);
-//     appendBtn(7);
-//     appendBtn(current_page, true);
-//   } else if (current_page <= per_page_max - 5) {
-//     appendBtn(current_page, true);
-//     appendBtn(current_page - 2);
-//     appendBtn(current_page - 1);
-//     appendBtn(current_page);
-//     appendBtn(current_page + 1);
-//     appendBtn(current_page + 2);
-//     appendBtn(current_page, true);
-//   } else {
-//     appendBtn(current_page, true);
-//     appendBtn(per_page_max - 6);
-//     appendBtn(per_page_max - 5);
-//     appendBtn(per_page_max - 4);
-//     appendBtn(per_page_max - 3);
-//     appendBtn(per_page_max - 2);
-//   }
-//   // это середина
-
-//   // это право
-//   appendBtn(per_page_max - 1);
-//   appendBtn(per_page_max);
-//   // это право
-// }
