da5c1941d0ffce5066895fade81ec09269fe5423
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2022-06-14 21:34:15 +0300

    fix bug preventDefault

--- src/js/home-modal-js/closingModal.js
+++ src/js/home-modal-js/closingModal.js
@@ -5,13 +5,11 @@ export function onKeyDown(event) {
   event.preventDefault();
   if (event.code === 'Escape') {
     onCloseModal();
-    window.removeEventListener('keydown', onKeyDown);
   }
 }
 
 export function onBackDropClick(event) {
   if (event.target === refs.modalBackdropRef) {
     onCloseModal();
-    document.removeEventListener('click', onBackDropClick);
   }
 }
