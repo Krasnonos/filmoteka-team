7af3c87b5dadb2c710a9706f5633b6d50a094c70
Sharlevych Dmytro <sharlevychdmytro@gmail.com>
2022-06-15 21:54:42 +0300

    fix bug popular

--- src/js/popular-movies/index.js
+++ src/js/popular-movies/index.js
@@ -1,4 +1,3 @@
-
 // import { requestPopularMovies } from './request-popular-movies';
 // import { createMarkup } from './create-markup';
 // import { refs } from '../refs-el/refs-el';
@@ -22,13 +21,17 @@ import { requestPopularMovies } from './request-popular-movies';
 import { createMarkup } from './create-markup';
 import { refs } from '../refs-el/refs-el';
 import { setResultData } from './data-result';
+import { renderPagination } from '../pagination/pagination';
 document.addEventListener('DOMContentLoaded', ready);
 
 async function ready() {
   try {
     const response = await requestPopularMovies(1);
     const results = response.data.results;
+    console.log(response);
     refs.galleryListEl.insertAdjacentHTML('afterbegin', createMarkup(results));
+    renderPagination(1, response.data.total_pages);
+
     setResultData(results);
   } catch (error) {
     refs.galleryListEl.insertAdjacentHTML(
