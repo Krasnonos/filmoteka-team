7f2c329d7ba3c0891e4a1731680b4160832ccdba
Krasnonos <andreykrasnonos@icloud.com>
2022-06-15 11:46:41 +0300

    done close modal-team

--- src/js/advice&team-modal/open-close-team-modal.js
+++ src/js/advice&team-modal/open-close-team-modal.js
@@ -7,6 +7,8 @@ openBtn.addEventListener('click', openModal);
 function openModal() {
   modalEl.classList.remove('is-hidden');
   closeBtn.addEventListener('click', closeModal);
+  window.addEventListener('keydown', closeModalByEcape);
+  modalEl.addEventListener('click', closeModalByBackdrop);
   openBtn.removeEventListener('click', openModal);
 }
 
@@ -14,4 +16,21 @@ function closeModal() {
   modalEl.classList.add('is-hidden');
   openBtn.addEventListener('click', openModal);
   closeBtn.removeEventListener('click', closeModal);
+  window.removeEventListener('keydown', closeModalByEcape);
+  modalEl.removeEventListener('click', closeModalByBackdrop);
+}
+
+function closeModalByEcape(e) {
+  e.preventDefault();
+
+  if (e.code === 'Escape') {
+    closeModal();
+  }
+}
+
+function closeModalByBackdrop(e) {
+  if (e.currentTarget !== e.target) {
+    return;
+  }
+  closeModal();
 }
