338833dcaec2d5ce66004cac020c15b904b71a9b
Krasnonos <andreykrasnonos@icloud.com>
2022-06-16 12:33:20 +0300

    add film to library

--- src/js/my-library/create-library-markup.js
+++ src/js/my-library/create-library-markup.js
@@ -1,21 +1,23 @@
 import hbs from '../../hbs-templates/film-card-library.hbs';
 import { genreIds } from '../popular-movies/ganre-ids';
 import { convertGanres } from '../popular-movies/convert-name-ganres';
+import imgNotFound from '../../images/image-not-found-adaptive.png';
 
 export default function createMarkup(results) {
   let markup = '';
 
   results.forEach(res => {
+    const ganres = res.genre_ids?.map(id => convertGanres(genreIds, id));
+    if (ganres.length > 2) {
+      ganres.length = 2;
+      ganres.push('Others');
+    }
     const data = {
       filmId: res.id,
       title: res.name || res.title || 'XXXX',
       urlImg: res.poster_path,
       relisYer: (res.release_date || res.first_air_date || 'XXXX').slice(0, 4),
-      ganres:
-        res.genres
-          .map(({ name }) => name)
-          .slice(0, 2)
-          .join(', ') || 'XXXX',
+      ganres: ganres.join(', ') || 'Others',
       ratting: res.vote_average,
     };
     markup += hbs(data);
