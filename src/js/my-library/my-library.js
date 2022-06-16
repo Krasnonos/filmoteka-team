2f8410012dfa676b55988f81565df6fd74088d14
Krasnonos <andreykrasnonos@icloud.com>
2022-06-16 17:35:28 +0300

    add pagination

--- src/js/my-library/my-library.js
+++ src/js/my-library/my-library.js
@@ -1,3 +1,5 @@
+import libraryPagination from './library-pagination';
+import getPartOfFilms from './get-part-of-films';
 import createMarkup from './create-library-markup';
 import { setCurrentFilmsData } from './current-films-data';
 
@@ -6,42 +8,52 @@ const queueBtn = document.querySelector('#queue');
 const galleryList = document.querySelector('.gallery-list');
 const watchedPlaceholder = document.querySelector('.js-watched-text');
 const queuePlaceholder = document.querySelector('.js-queue-text');
+let currentPage = 1;
 
 queueBtn.addEventListener('click', showFilms);
 watchedBtn.addEventListener('click', showFilms);
-document.addEventListener('DOMContentLoaded', firstShowFilms);
+document.addEventListener('DOMContentLoaded', showFilms);
+
+// function showFilms(e) {
+//   const localStorageKey = e.currentTarget.dataset.key;
+//   const filmsArrayJson = localStorage.getItem(localStorageKey);
+//   const filmsArray = JSON.parse(filmsArrayJson);
+
+//   if (!filmsArray) {
+//     galleryList.innerHTML = '';
+//     return;
+//   }
+
+//   galleryList.innerHTML = createMarkup(filmsArray);
+//   setCurrentFilmsData(filmsArray);
+//   hidePlaceholder(localStorageKey);
+// }
 
 function showFilms(e) {
-  const localStorageKey = e.currentTarget.dataset.key;
+  const currentSection = document.querySelector(
+    'button[data-current="currentPage"]'
+  );
+  const localStorageKey = currentSection.dataset.key;
   const filmsArrayJson = localStorage.getItem(localStorageKey);
-  const filmsArray = JSON.parse(filmsArrayJson);
+  const filmsArrayAll = JSON.parse(filmsArrayJson);
+  const partOfFilms = getPartOfFilms(filmsArrayAll, currentPage);
+  const totalPage = Math.ceil(filmsArrayAll.length / 20);
 
-  if (!filmsArray) {
+  if (!filmsArrayAll) {
     galleryList.innerHTML = '';
     return;
   }
 
-  galleryList.innerHTML = createMarkup(filmsArray);
-  setCurrentFilmsData(filmsArray);
-  hidePlaceholder(localStorageKey);
-}
-
-function firstShowFilms(e) {
-  const localStorageKey = watchedBtn.dataset.key;
-  const filmsArrayJson = localStorage.getItem(localStorageKey);
-  const filmsArray = JSON.parse(filmsArrayJson);
-
-  if (!filmsArray) {
-    galleryList.innerHTML = '';
-    return;
+  if (filmsArrayAll.length > 20) {
+    libraryPagination(currentPage, totalPage);
   }
 
-  galleryList.innerHTML = createMarkup(filmsArray);
-  setCurrentFilmsData(filmsArray);
+  galleryList.innerHTML = createMarkup(partOfFilms);
+  setCurrentFilmsData(partOfFilms);
   hidePlaceholder(localStorageKey);
 }
 
-function hidePlaceholder(key) {
+export default function hidePlaceholder(key) {
   if (key === `watched-films`) {
     watchedPlaceholder.style.display = 'none';
   } else {
--- src/library.js
+++ src/library.js
@@ -1,4 +1,4 @@
-import './js/home-modal-js/homeModalJS';
+// import './js/home-modal-js/homeModalJS';
 import './js/clear-local-storage-library.js/change-current-page';
 import './js/my-library/my-library';
 import './js/spinner-js/spinner';
@@ -9,4 +9,4 @@ import './js/theme-change/day-night';
 // import './js/my-library-home/index';
 import './js/clear-local-storage-library.js/clear-local-storage';
 import './js/clear-local-storage-library.js/clear-one-card';
-import './js/firebase/logout-library'
+import './js/firebase/logout-library';
