66ef57a8d61010f8c4a1c3f17af7b7945a3178b7
Krasnonos <andreykrasnonos@icloud.com>
2022-06-17 12:42:21 +0300

    fix advice form

--- src/js/advice&team-modal/advice-films-by-ganre.js
+++ src/js/advice&team-modal/advice-films-by-ganre.js
@@ -1,5 +1,6 @@
 import searchFilmByGanre from './search-film-by-ganre';
 import randomFilm from './random-film';
+import { setResultData } from '../popular-movies/data-result';
 import { getModalData } from '../home-modal-js/getModalData';
 import { showModal } from '../home-modal-js/showModal';
 import { spinnerOff } from '../spinner-js/spinner';
@@ -22,8 +23,10 @@ async function adviceFilmByGanre(e) {
     const oneRandomFilm = randomFilm(adviceFilmsArray);
     const randomFilmId = oneRandomFilm.id;
     const filmInformation = await getModalData(randomFilmId);
+
     showModal(filmInformation);
     spinnerOff();
+    setResultData(adviceFilmsArray);
   } catch (erroe) {
     Notify.info('We are sorry but this genre is empty.');
   }
