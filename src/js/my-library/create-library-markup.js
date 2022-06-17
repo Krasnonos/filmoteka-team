2e771f07f9a66c4c9182895bea793201de651f3d
Sharlevych Dmytro <sharlevychdmytro@gmail.com>
2022-06-17 13:03:48 +0300

    fix bug valid img

--- src/js/my-library/create-library-markup.js
+++ src/js/my-library/create-library-markup.js
@@ -8,16 +8,29 @@ export default function createMarkup(results) {
   let markup = '';
 
   results.forEach(res => {
-    const ganres = res.genre_ids?.map(id => convertGanres(genreIds, id));
+    const ganres = res.genre_ids
+      ? res.genre_ids.map(id => convertGanres(genreIds, id))
+      : ['Others'];
+
     if (ganres.length > 2) {
       ganres.length = 2;
       ganres.push('Others');
+    }
+
+    let imgValid = '';
+
+    if (res.poster_path) {
+      imgValid = `https://image.tmdb.org/t/p/w300${res.poster_path}`;
+    } else if (res.profile_path) {
+      imgValid = `https://image.tmdb.org/t/p/w300${res.profile_path}`;
+    } else {
+      imgValid = imgNotFound;
     }
     const data = {
       svg,
       filmId: res.id,
       title: res.name || res.title || 'XXXX',
-      urlImg: res.poster_path,
+      urlImg: imgValid,
       relisYer: (res.release_date || res.first_air_date || 'XXXX').slice(0, 4),
       ganres: ganres.join(', ') || 'Others',
       ratting: res.vote_average,
